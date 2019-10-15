e7526257715ca071566d6c8fa854084d93abd61a
devNica <desingsdevnicaragua@gmail.com>
2019-10-14 23:41:03 -0600

    express-validator

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose')
 var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
+var validator = require('express-validator');
 
 
 var indexRouter = require('./routes/index');
@@ -24,6 +25,7 @@ app.set('view engine', '.hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({ secret: 'mySecretPass', resave: false, saveUninitialized: false }));
 app.use(flash());
--- config/passport.js
+++ config/passport.js
@@ -17,6 +17,20 @@ passport.use('local.signup', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true,
 }, function (req, email, password, done) {
+
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty().isLength({ min: 6 });
+    var errors = req.validationErrors();
+
+    if (errors) {
+        var messages = [];
+        errors.forEach(error => {
+            messages.push(error.msg);
+        });
+
+        return done(null, false, req.flash('error', messages));
+    }
+
     User.findOne({ 'email': email }, function (err, user) {
         if (err) {
             return done(err);
